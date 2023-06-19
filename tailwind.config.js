7d4fede6bc67f9941cc41f45171b9ea0e8ec03ea
calumjoyce <calumjoyce74@gmail.com>
2023-06-19 22:48:32 +0100

    build: tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#FFDEE9",
+        ocean: "#B5FFFC"
+      }
+    },
+  },
+  plugins: [],
+}
